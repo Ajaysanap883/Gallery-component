c3d625f09c4d9b28b682af1125fceffe8a8d1ac3
Ajaysanap883 <ajaysanap884@gmail.com>
2024-04-24 13:40:00 +0530

    first commit

---
+++ src/Components/Gallery/Gallery.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Gallery.css';
+import Herosection from './herosection';
+import Grid from './grid';
+// import Band from './band';
+
+const Gallery = () => {
+  return (
+  <>
+    <Herosection/>
+    <Grid/>
+    {/* <Band/> */}
+  </>
+  )
+}
+
+export default Gallery
---
+++ src/Components/Gallery/band.js
@@ -0,0 +1,8 @@
+export default function Example() {
+    return (
+      <div className="bg-zinc-900 mb-10 h-[500px] py-24 sm:py-32">
+        
+      </div>
+    )
+  }
+  
